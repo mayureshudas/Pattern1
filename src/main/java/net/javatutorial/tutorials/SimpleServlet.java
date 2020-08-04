5712d186d6a6047e4d313c9e504a166e7844150c
mayureshudas <mayuresh.udas@capgemini.com>
2020-08-05 00:37:21 +0530

    Update SimpleServlet.java

--- src/main/java/net/javatutorial/tutorials/SimpleServlet.java
+++ src/main/java/net/javatutorial/tutorials/SimpleServlet.java
@@ -13,7 +13,7 @@ public class SimpleServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().println("Hello World!");
+		response.getWriter().println("Hello World! Welcome to pattern demo");
 	}
 	
 	@Override
