ec1adfdb3042284af23f2a0eca23216c0e434e88
mayureshudas <mayuresh.udas@capgemini.com>
2020-08-05 00:13:50 +0530

    Update run.groovy

--- vars/run.groovy
+++ vars/run.groovy
@@ -1,6 +1,6 @@
 def runproject(){
 
-       sh "cp -fR target/petclinic.war /var/lib/tomcat/webapps"
+       sh "cp -fR target/SimpleServlet-1.war /var/lib/tomcat/webapps"
        sh "service tomcat restart"
        
 }
